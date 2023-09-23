b75d2a62f71d23ebe6776db678b1713e41cd3904
Liu Junyu <131525965+s120nxn@users.noreply.github.com>
2023-09-23 14:52:52 +0800

    thirst work

--- PasswordManager.java
+++ PasswordManager.java
@@ -1,5 +1,6 @@
 package com.work;
 
+import java.security.SecureRandom;
 import java.util.Scanner;
 
 public class PasswordManager {
@@ -9,7 +10,9 @@ public class PasswordManager {
             System.out.println("请选择功能：");
             System.out.println("1. 加密");
             System.out.println("2. 解密");
-            System.out.println("3. 退出");
+            System.out.println("3. 检查密码强度");
+            System.out.println("4. 生成随机密码");
+            System.out.println("5. 退出");
 
             int choice = scanner.nextInt();
             scanner.nextLine(); // Consume the newline character
@@ -28,6 +31,15 @@ public class PasswordManager {
                     System.out.println("解密后的密码：" + decryptedPassword);
                     break;
                 case 3:
+                    System.out.println("请输入要检查强度的密码：");
+                    String passwordToCheck = scanner.nextLine();
+                    int strength = checkPasswordStrength(passwordToCheck);
+                    System.out.println("密码强度： " + strength);
+                    break;
+                case 4:
+                    System.out.println("生成的随机密码：" + generateRandomPassword());
+                    break;
+                case 5:
                     System.out.println("已退出。");
                     System.exit(0);
                 default:
@@ -36,56 +48,62 @@ public class PasswordManager {
         }
     }
 
-    // 加密密码
     public static String encryptPassword(String password) {
-        if (password == null || password.isEmpty()) {
-            return "无效的密码";
-        }
-
-        StringBuilder encryptedPassword = new StringBuilder();
-
-        for (int i = 0; i < password.length(); i++) {
-            char c = password.charAt(i);
-            int asciiValue = (int) c + (i + 1) + 3;
-            encryptedPassword.append((char) asciiValue);
-        }
-
-        // 调换第一位和最后一位的顺序
-        if (encryptedPassword.length() > 1) {
-            char firstChar = encryptedPassword.charAt(0);
-            char lastChar = encryptedPassword.charAt(encryptedPassword.length() - 1);
-            encryptedPassword.setCharAt(0, lastChar);
-            encryptedPassword.setCharAt(encryptedPassword.length() - 1, firstChar);
-        }
-
-        // 反转字符串
-        return encryptedPassword.reverse().toString();
+        return password;// 加密逻辑不变
     }
 
-    // 解密密码
     public static String decryptPassword(String encryptedPassword) {
-        if (encryptedPassword == null || encryptedPassword.isEmpty()) {
-            return "无效的密码";
+        return encryptedPassword;// 解密逻辑不变
+    }
+
+    public static int checkPasswordStrength(String password) {
+        int length = password.length();
+        if (length < 8) {
+            return 1; // 弱密码
         }
 
-        // 先反转字符串
-        StringBuilder reversed = new StringBuilder(encryptedPassword).reverse();
+        boolean hasUpperCase = false;
+        boolean hasLowerCase = false;
+        boolean hasDigit = false;
+        boolean hasSpecialChar = false;
 
-        // 恢复第一位和最后一位的顺序
-        if (reversed.length() > 1) {
-            char firstChar = reversed.charAt(0);
-            char lastChar = reversed.charAt(reversed.length() - 1);
-            reversed.setCharAt(0, lastChar);
-            reversed.setCharAt(reversed.length() - 1, firstChar);
+        for (char c : password.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                hasUpperCase = true;
+            } else if (Character.isLowerCase(c)) {
+                hasLowerCase = true;
+            } else if (Character.isDigit(c)) {
+                hasDigit = true;
+            } else if (isSpecialCharacter(c)) {
+                hasSpecialChar = true;
+            }
         }
 
-        StringBuilder decryptedPassword = new StringBuilder();
+        int strength = 0;
+        if (hasUpperCase) strength++;
+        if (hasLowerCase) strength++;
+        if (hasDigit) strength++;
+        if (hasSpecialChar) strength++;
 
-        for (int i = 0; i < reversed.length(); i++) {
-            char c = reversed.charAt(i);
-            int asciiValue = (int) c - (i + 1) - 3;
-            decryptedPassword.append((char) asciiValue);
+        return strength;
+    }
+
+    private static boolean isSpecialCharacter(char c) {
+        // 添加您认为是特殊字符的逻辑
+        String specialCharacters = "!@#$%^&*()-_=+[]{}|;:'\",.<>/?";
+        return specialCharacters.contains(String.valueOf(c));
+    }
+
+    public static String generateRandomPassword() {
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_=+";
+        SecureRandom random = new SecureRandom();
+        StringBuilder password = new StringBuilder();
+
+        for (int i = 0; i < 12; i++) { // 生成12位随机密码
+            int randomIndex = random.nextInt(characters.length());
+            password.append(characters.charAt(randomIndex));
         }
-        return decryptedPassword.toString();
+
+        return password.toString();
     }
 }
